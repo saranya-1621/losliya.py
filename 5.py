4a3fecde67ab42dd044d4005d59128641e827e14
saranya-1621 <51949644+saranya-1621@users.noreply.github.com>
2019-07-06 19:53:49 +0530

    Update 5.py

--- 5.py
+++ 5.py
@@ -1,11 +1,11 @@
-afs=input()
-ahs=1
-for d1 in range (len(afs)-1):
-  aph=afs[d1]+afs[d1+1]
-  m1=int(aph)
-  if m1<=26 and afs[d1]!="0":
-      ahs+=1
-if ahs==3:
-  print(ahs)
+f1=input()
+h1=1
+for d1 in range (len(f1)-1):
+  hp=f1[d1]+f1[d1+1]
+  m1=int(hp)
+  if m1<=26 and f1[d1]!="0":
+      h1+=1
+if h1==3:
+  print(h1)
 else:
-  print(ahs+(ahs-1)//2)
+  print(h1+(h1-1)//2)
